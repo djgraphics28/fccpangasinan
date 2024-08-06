705e20ee4f46da92cf4d17df4ab33fa330817c9f
djgraphics28 <darwin.ibay30@gmail.com>
2024-08-06 13:29:05 +0800

    updates

--- app/Filament/Resources/ParticipantResource/Widgets/ChurchParticipantCountOverview.php
+++ app/Filament/Resources/ParticipantResource/Widgets/ChurchParticipantCountOverview.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ParticipantResource\Widgets;
 
 use App\Models\Church;
+use App\Models\Participant;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
@@ -10,17 +11,30 @@ class ChurchParticipantCountOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        $churches = Church::all()->count();
-
-        // foreach ($churches as $church) {
-        //     $stats[] = Stat::make($church->name, $church->participants()->count())->icon('heroicon-o-users');
-        // }
+        $all = Participant::all()->count();
+        $bugayong = Participant::where('church_id', 1)->count();
+        $balangobong = Participant::where('church_id', 2)->count();
+        $sanBonifacio = Participant::where('church_id', 3)->count();
+        $rosales = Participant::where('church_id', 4)->count();
+        $basista = Participant::where('church_id', 5)->count();
+        $labrador = Participant::where('church_id', 6)->count();
+        $mabini = Participant::where('church_id', 7)->count();
+        $alcalaApartado = Participant::where('church_id', 8)->count();
+        $alcalaAnulid = Participant::where('church_id', 9)->count();
+        $alcalaBokil = Participant::where('church_id', 10)->count();
 
         return [
-            Stat::make('Total Requests', $churches)
-                // ->description('The total number of Requests')
-                ->icon('heroicon-o-users')
-
+            Stat::make('All', $all)->icon('heroicon-o-users'),
+            Stat::make('FCC Bugayong', $bugayong)->icon('heroicon-o-users'),
+            Stat::make('FCC Balangobong', $balangobong)->icon('heroicon-o-users'),
+            Stat::make('FCC San Bonifacio', $sanBonifacio)->icon('heroicon-o-users'),
+            Stat::make('FCC Rosales', $rosales)->icon('heroicon-o-users'),
+            Stat::make('FCC Basista', $basista)->icon('heroicon-o-users'),
+            Stat::make('FCC Labrador', $labrador)->icon('heroicon-o-users'),
+            Stat::make('FCC Mabini', $mabini)->icon('heroicon-o-users'),
+            Stat::make('FCC Alcala (Apartado)', $alcalaApartado)->icon('heroicon-o-users'),
+            Stat::make('FCC Alcala (Anulid)', $alcalaAnulid)->icon('heroicon-o-users'),
+            Stat::make('FCC Alcala (Bokil)', $alcalaBokil)->icon('heroicon-o-users'),
         ];
     }
 }
